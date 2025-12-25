a994c112a425e542f27c088bace16f43a88c58bd
sasbbb <164556854+sasbbb@users.noreply.github.com>
2025-12-25 00:58:27 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#btn").addEventListener("click", () => {
     alert("hi");
   });
-//});
+});
